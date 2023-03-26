9fdc5be00ee370129caf16b1025ca494ffeac901
hphilosopy <115016616+hphilosopy@users.noreply.github.com>
2023-03-26 21:44:02 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -1,14 +1,9 @@
 export const defaultSetting = {
   continuousDialogue: true,
   archiveSession: false,
-  openaiAPIKey: "",
+  openaiAPIKey: "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",
   openaiAPITemperature: 60,
   systemRule: ""
 }
 
-export const defaultMessage = `
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 提供支持。
-- 关注公众号[noarsark](https://imagecdn.noarsark.cn/blog/202302152115832.png)，回复【chatgpt】获取体验key。
-- 访问人数较多时，会报错Error，请重新刷新后再提问。如想流畅体验可以联系[微信noarsark18](https://imagecdn.noarsark.cn/blog/chatAI.jpg)购买独享账号或plus账号。
-- 欢迎加入[ChatGPT交流群](https://imagecdn.noarsark.cn/blog/202303252114707.jpg)，一起探索ChatGPT最新玩法。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+export const defaultMessage = `由洪某某提供维护`
